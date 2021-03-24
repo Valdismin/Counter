b14441d93c559ab83efde364eeea9215ad748685
Valdismin <Valdismin@gmail.com>
2021-03-24 18:00:29 +0300

    Updates

---
+++ precache-manifest.bc9bafd41d8d1a7bcdbd437ee39cfc02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35695b877f903b4b3551dae8c75451d9",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "0df0fecde2b17ec4c213",
+    "url": "/Counter/static/css/main.b39e671e.chunk.css"
+  },
+  {
+    "revision": "6a369545800405b18104",
+    "url": "/Counter/static/js/2.4aaae195.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Counter/static/js/2.4aaae195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df0fecde2b17ec4c213",
+    "url": "/Counter/static/js/main.f32986a1.chunk.js"
+  },
+  {
+    "revision": "8290f2e6b6e6218d3651",
+    "url": "/Counter/static/js/runtime-main.c5ebaff2.js"
+  }
+]);
